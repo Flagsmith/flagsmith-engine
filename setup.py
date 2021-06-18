e899137aaa785f156efc6514a4d69b33d406c2db
Matthew Elwell <matthewe@solidstategroup.com>
2021-06-18 17:59:18 +0100

    Get tox working properly

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-requirements = [
-    "marshmallow"
-]
-
 
 setup(
     name='Flagsmith Flag Engine',
     version='0.1.0',
     author='Matthew Elwell',
     author_email='matthew.elwell@flagsmith.com',
-    packages=['flag_engine', 'flag_engine.test'],
+    packages=['flag_engine'],
     url='http://pypi.python.org/pypi/flagsmith-flag-engine/',
     license='LICENSE.txt',
     description='Flag engine for the Flagsmith API.',
