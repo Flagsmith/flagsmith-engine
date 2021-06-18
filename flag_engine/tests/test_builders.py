64cbf0384766bf72ef55cef41e4bbab090b85797
Matthew Elwell <matthewe@solidstategroup.com>
2021-06-18 15:41:44 +0100

    Rename tests directory and add test for dictionary conversion of environment

--- flag_engine/tests/test_builders.py
+++ flag_engine/tests/test_builders.py
@@ -35,3 +35,41 @@ def test_get_flags_for_environment_returns_feature_states_for_django_environment
         isinstance(feature_state, FeatureState)
         for feature_state in environment_model.feature_states
     )
+
+
+def test_get_flags_for_environment_returns_feature_states_for_environment_dictionary():
+    # Given
+    # a dictionary to represent a Flagsmith environment
+    environment_dict = {
+        "id": 1,
+        "api_key": "api-key",
+        "project": {"id": 1, "name": "test project"},
+        "feature_states": [
+            {
+                "id": 1,
+                "enabled": True,
+                "value": None,
+                "feature": {"id": 1, "name": "enabled_feature"},
+            },
+            {
+                "id": 2,
+                "enabled": False,
+                "value": None,
+                "feature": {"id": 2, "name": "disabled_feature"},
+            },
+        ],
+    }
+
+    # When
+    # we build the environment model
+    environment_model = build_environment_model(environment_dict)
+
+    # Then
+    # the returned object is an Environment object
+    assert isinstance(environment_model, Environment)
+
+    # and each of the feature states are FeatureState objects
+    assert all(
+        isinstance(feature_state, FeatureState)
+        for feature_state in environment_model.feature_states
+    )
