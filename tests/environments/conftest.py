7ce391b4bc5504bc6fc82f09d1021cbd8b312128
Matthew Elwell <matthewe@solidstategroup.com>
2021-06-30 10:55:04 +0100

    Fix builder test

--- tests/environments/conftest.py
+++ tests/environments/conftest.py
@@ -1,34 +1,48 @@
-from unittest import mock
+from dataclasses import dataclass
 
 import pytest
 
 
+@dataclass
+class Project:
+    id: int
+    name: str
+
+
 @pytest.fixture()
 def mock_project():
-    mock_project = mock.MagicMock(id=1)
-    mock_project.name = "Test Project"  # `name` has meaning on mock init
-    return mock_project
+    return Project(1, "Test Project")
+
+
+@dataclass
+class Feature:
+    id: int
+    name: str
+    project: Project
 
 
 @pytest.fixture()
 def mock_feature_1(mock_project):
-    mock_feature = mock.MagicMock(id=1, project=mock_project)
-    mock_feature.name = "feature_1"
-    return mock_feature
+    return Feature(id=1, project=mock_project, name="feature_1")
 
 
 @pytest.fixture()
 def mock_feature_2(mock_project):
-    mock_feature = mock.MagicMock(id=2, project=mock_project)
-    mock_feature.name = "feature_2"
-    return mock_feature
+    return Feature(id=2, project=mock_project, name="feature_2")
+
+
+@dataclass
+class FeatureState:
+    id: int
+    feature: Feature
+    enabled: bool
 
 
 @pytest.fixture()
 def mock_enabled_feature_state(mock_feature_1):
-    return mock.MagicMock(id=1, feature=mock_feature_1, enabled=True)
+    return FeatureState(id=1, feature=mock_feature_1, enabled=True)
 
 
 @pytest.fixture()
 def mock_disabled_feature_state(mock_feature_2):
-    return mock.MagicMock(id=1, feature=mock_feature_2, enabled=False)
+    return FeatureState(id=2, feature=mock_feature_2, enabled=False)
--- tests/environments/test_environments_builders.py
+++ tests/environments/test_environments_builders.py
@@ -1,5 +1,3 @@
-from unittest import mock
-
 from flag_engine.environments.builders import build_environment_model
 from flag_engine.environments.models import Environment
 from flag_engine.features.models import FeatureState
@@ -10,29 +8,27 @@ def test_get_flags_for_environment_returns_feature_states_for_django_environment
 ):
     # Given
     # a mock environment object to simulate a Django environment object
-    mock_fs_related_manager = mock.MagicMock()
-    mock_fs_related_manager.all.return_value = [
-        mock_disabled_feature_state,
-        mock_enabled_feature_state,
-    ]
-    mock_environment = mock.MagicMock(
-        id=1,
-        api_key="api-key",
-        project=mock_project,
-        feature_states=mock_fs_related_manager,
-    )
-    mock_environment.name = "Test Environment"  # `name` has meaning on mock init
+    class MockEnvironment:
+        class MockObjectManager:
+            def all(self):
+                return [mock_disabled_feature_state, mock_enabled_feature_state]
+
+        id = 1
+        name = "Test Environment"
+        api_key = "api-key"
+        project = mock_project
+        feature_states = MockObjectManager()
 
     # When
     # we build the environment model
-    environment_model = build_environment_model(mock_environment)
+    environment_model = build_environment_model(MockEnvironment())
 
     # Then
     # the returned object is an Environment object
     assert isinstance(environment_model, Environment)
 
     # and each of the feature states are FeatureState objects
-    assert all(
+    assert len(environment_model.feature_states) == 2 and all(
         isinstance(feature_state, FeatureState)
         for feature_state in environment_model.feature_states
     )
@@ -70,7 +66,7 @@ def test_get_flags_for_environment_returns_feature_states_for_environment_dictio
     assert isinstance(environment_model, Environment)
 
     # and each of the feature states are FeatureState objects
-    assert all(
+    assert len(environment_model.feature_states) == 2 and all(
         isinstance(feature_state, FeatureState)
         for feature_state in environment_model.feature_states
     )
