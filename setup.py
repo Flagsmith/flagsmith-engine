48beaa93d4f687d068e557ce835c86c45afa24e1
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-09-01 15:27:27 +0530

    include nested packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Flagsmith Flag Engine",
     version="0.1.0",
     author="Matthew Elwell",
     author_email="matthew.elwell@flagsmith.com",
-    packages=["flag_engine"],
+    packages=find_packages(include=["flag_engine", "flag_engine.*"]),
     url="http://pypi.python.org/pypi/flagsmith-flag-engine/",
     license="LICENSE.txt",
     description="Flag engine for the Flagsmith API.",
