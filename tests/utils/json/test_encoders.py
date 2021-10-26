51235c45ea228a3b0975752ebc0c7a54b60f2dbd
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-10-26 18:24:47 +0530

    squash! black format it

--- tests/utils/json/test_encoders.py
+++ tests/utils/json/test_encoders.py
@@ -5,12 +5,18 @@ from flag_engine.utils.json.encoders import DecimalEncoder
 
 def test_decimal_encoder_converts_decimal():
     # Given
-    data = {"int_decimal": Decimal(1), "float_decimal": Decimal("1.99"), "str_value": "string"}
+    data = {
+        "int_decimal": Decimal(1),
+        "float_decimal": Decimal("1.99"),
+        "str_value": "string",
+    }
 
     # when
     json_data = json.dumps(data, cls=DecimalEncoder)
 
     # Then
-    assert json.loads(json_data) == {"int_decimal": 1, "float_decimal": 1.99, "str_value": "string"}
-
-
+    assert json.loads(json_data) == {
+        "int_decimal": 1,
+        "float_decimal": 1.99,
+        "str_value": "string",
+    }
