a8ea4d25f3dfa6f0e1d9be8cb12ef0b7a38795c7
Matthew Elwell <matthewe@solidstategroup.com>
2021-11-25 13:19:02 +0000

    Add update traits functionality (#34)

--- tests/identities/test_identities_models.py
+++ tests/identities/test_identities_models.py
@@ -1,4 +1,4 @@
-from flag_engine.identities.models import IdentityModel
+from flag_engine.identities.models import IdentityModel, TraitModel
 
 
 def test_composite_key():
@@ -25,3 +25,41 @@ def test_generate_composite_key():
         )
         == f"{environment_api_key}_{identifier}"
     )
+
+
+def test_update_traits_remove_traits_with_none_value(identity_in_segment):
+    # Given
+    trait_key = identity_in_segment.identity_traits[0].trait_key
+    trait_to_remove = TraitModel(trait_key=trait_key, trait_value=None)
+
+    # When
+    identity_in_segment.update_traits([trait_to_remove])
+
+    # Then
+    assert identity_in_segment.identity_traits == []
+
+
+def test_update_identity_traits_updates_trait_value(identity_in_segment):
+    # Given
+    trait_key = identity_in_segment.identity_traits[0].trait_key
+    trait_value = "updated_trait_value"
+    trait_to_update = TraitModel(trait_key=trait_key, trait_value=trait_value)
+
+    # When
+    identity_in_segment.update_traits([trait_to_update])
+
+    # Then
+    assert len(identity_in_segment.identity_traits) == 1
+    assert identity_in_segment.identity_traits[0] == trait_to_update
+
+
+def test_update_traits_adds_new_traits(identity_in_segment):
+    # Given
+    new_trait = TraitModel(trait_key="new_key", trait_value="foobar")
+
+    # When
+    identity_in_segment.update_traits([new_trait])
+
+    # Then
+    assert len(identity_in_segment.identity_traits) == 2
+    assert new_trait in identity_in_segment.identity_traits
