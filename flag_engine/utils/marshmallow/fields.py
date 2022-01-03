fd3726bdbe8aaa945046e320f49dbebc0a970d55
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-01-03 17:33:56 +0530

    fixup: remove type hint
    because 'ABCMeta' object is not subscriptable for py<3.9

--- flag_engine/utils/marshmallow/fields.py
+++ flag_engine/utils/marshmallow/fields.py
@@ -1,12 +1,8 @@
-import typing
-
 from marshmallow import fields
 
 from flag_engine.utils.collections import IdentityFeaturesList
 
 
 class IdentityFeaturesListField(fields.List):
-    def _deserialize(
-        self, value, attr, data, **kwargs
-    ) -> IdentityFeaturesList[typing.Any]:
+    def _deserialize(self, value, attr, data, **kwargs) -> IdentityFeaturesList:
         return IdentityFeaturesList(super()._deserialize(value, attr, data, **kwargs))
