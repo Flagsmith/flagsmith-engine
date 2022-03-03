23a34ef17034024df6a28b88004407a193bd92c7
Matthew Elwell <matthewe@solidstategroup.com>
2022-03-03 16:56:26 +0000

    Add document builder tests

--- tests/unit/django_transform/test_document_builders.py
+++ tests/unit/django_transform/test_document_builders.py
@@ -1,11 +1,19 @@
 import json
+from datetime import datetime, timedelta
 
 from flag_engine.django_transform.document_builders import (
     build_environment_api_key_document,
     build_environment_document,
     build_identity_document,
 )
+from flag_engine.features.constants import STANDARD
 from flag_engine.utils.json.encoders import DecimalEncoder
+from tests.mock_django_classes import (
+    DjangoEnvironment,
+    DjangoFeature,
+    DjangoFeatureState,
+    DjangoIdentity,
+)
 
 
 def test_build_identity_document(django_identity):
@@ -71,4 +79,63 @@ def test_build_environment_api_key_document(django_environment_api_key):
     assert (
         api_key_document["client_api_key"]
         == django_environment_api_key.environment.api_key
+    )
+
+
+def test_build_environment_document_with_multiple_feature_state_versions(
+    django_project,
+):
+    # Given
+    yesterday = datetime.now() - timedelta(days=1)
+
+    feature = DjangoFeature(
+        id=1, name="test_feature", project=django_project, type=STANDARD
+    )
+    default_fs_kwargs = {"feature": feature, "live_from": yesterday, "enabled": True}
+    feature_state_v1 = DjangoFeatureState(id=1, version=1, **default_fs_kwargs)
+    feature_state_v2 = DjangoFeatureState(id=2, version=2, **default_fs_kwargs)
+
+    django_environment = DjangoEnvironment(
+        id=1,
+        project=django_project,
+        feature_states=[feature_state_v1, feature_state_v2],
+    )
+
+    # When
+    environment_document = build_environment_document(django_environment)
+
+    # Then
+    assert len(environment_document["feature_states"]) == 1
+    assert environment_document["feature_states"][0]["django_id"] == feature_state_v2.id
+
+
+def test_build_identity_document_with_multiple_feature_state_versions(
+    django_environment, django_disabled_feature_state
+):
+    # Given
+    yesterday = datetime.now() - timedelta(days=1)
+    feature = django_disabled_feature_state.feature
+
+    identity_feature_state_v1 = DjangoFeatureState(
+        id=2, version=1, feature=feature, live_from=yesterday, enabled=True
+    )
+    identity_feature_state_v2 = DjangoFeatureState(
+        id=3, version=2, feature=feature, live_from=yesterday, enabled=True
+    )
+    django_identity = DjangoIdentity(
+        id=1,
+        identifier="identity",
+        created_date=yesterday,
+        environment=django_environment,
+        feature_states=[identity_feature_state_v1, identity_feature_state_v2],
+    )
+
+    # When
+    identity_document = build_identity_document(django_identity)
+
+    # Then
+    assert len(identity_document["identity_features"]) == 1
+    assert (
+        identity_document["identity_features"][0]["django_id"]
+        == identity_feature_state_v2.id
     )
