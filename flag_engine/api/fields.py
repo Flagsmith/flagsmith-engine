d5b10510fac88a3f0172ed0c728e5c71122cef74
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-19 16:43:10 +0530

    wip: Add api module

---
+++ flag_engine/api/fields.py
@@ -0,0 +1,40 @@
+import typing
+from decimal import Decimal
+
+from marshmallow.fields import Field
+
+from .constants import ACCEPTED_TRAIT_VALUE_TYPES
+
+
+class APITraitValueField(Field):
+    def _serialize(self, value: typing.Any, attr: str, obj: typing.Any, **kwargs):
+        return value
+
+    def _deserialize(
+        self,
+        value: typing.Any,
+        attr: typing.Optional[str],
+        data: typing.Optional[typing.Mapping[str, typing.Any]],
+        **kwargs,
+    ):
+        if type(value) not in ACCEPTED_TRAIT_VALUE_TYPES:
+            value = str(value)
+        return value
+
+
+class DynamoDBTraitValueField(Field):
+    def _deserialize(
+        self,
+        value: typing.Any,
+        attr: typing.Optional[str],
+        data: typing.Optional[typing.Mapping[str, typing.Any]],
+        **kwargs,
+    ):
+        if type(value) == Decimal:
+            return float(value)
+        return value
+
+    def _serialize(self, value: typing.Any, attr: str, obj: typing.Any, **kwargs):
+        if type(value) == float:
+            return Decimal(value)
+        return value
---
+++ flag_engine/api/schemas.py
@@ -0,0 +1,27 @@
+import typing
+
+from marshmallow import ValidationError, validates
+
+from flag_engine.identities.schemas import TraitSchema
+
+from .constants import TRAIT_STRING_VALUE_MAX_LENGTH
+from .fields import APITraitValueField, DynamoDBTraitValueField
+
+
+class APITraitSchema(TraitSchema):
+    trait_value = APITraitValueField(allow_none=True)
+
+    @validates("trait_value")
+    def validate_trait_value_length(
+        self, trait_value: typing.Union[int, str, bool, float]
+    ):
+        type_ = type(trait_value)
+        if type_ == str and len(trait_value) > TRAIT_STRING_VALUE_MAX_LENGTH:
+            raise ValidationError(
+                f"Value string is too long. Must be less than "
+                f"{TRAIT_STRING_VALUE_MAX_LENGTH} character"
+            )
+
+
+class DynamoDBTraitSchema(TraitSchema):
+    trait_value = DynamoDBTraitValueField(allow_none=True)
