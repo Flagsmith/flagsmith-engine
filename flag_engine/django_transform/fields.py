083c2f66f422661bae6303c446592492469846b4
Matthew Elwell <matthewe@solidstategroup.com>
2022-04-20 15:24:53 +0100

    Handle feature state versioning (#100)
    * Handle versioned feature states
    
    * Add test for new field
    
    * Add document builder tests
    
    * Update tests to include a feature state that is not yet live

--- flag_engine/django_transform/fields.py
+++ flag_engine/django_transform/fields.py
@@ -1,4 +1,5 @@
 import typing
+from datetime import datetime
 
 from marshmallow import fields
 
@@ -8,14 +9,29 @@ class DjangoRelatedManagerField(fields.List):
         super().__init__(*args, **kwargs)
         self.filter_func = filter_func
 
-    def _serialize(
-        self, value, *args, **kwargs
-    ) -> typing.Optional[typing.List[typing.Any]]:
+    def get_value(self, obj, attr, **kwargs) -> typing.List[typing.Any]:
         # indicates that the object was passed from django and the 'list' is
         # actually a related manager field, so grab all of the related objects
         # and filter them as necessary (this is done outside of the django ORM to
         # allow for prefetch related optimisations).
-        value = list(value.all())
+        value = list(getattr(obj, attr).all())
         if self.filter_func:
             value = list(filter(self.filter_func, value))
-        return super(DjangoRelatedManagerField, self)._serialize(value, *args, **kwargs)
+        return value
+
+
+class DjangoFeatureStatesRelatedManagerField(DjangoRelatedManagerField):
+    def get_value(self, obj, attr, **kwargs):
+        now = datetime.now()
+        features_map = {}
+        for fs in getattr(obj, attr).all():
+            if self.filter_func and not self.filter_func(fs):
+                continue
+
+            existing_feature_state = features_map.get(fs.feature_id)
+            if not existing_feature_state or (
+                fs.live_from < now and fs.version > existing_feature_state.version
+            ):
+                features_map[fs.feature_id] = fs
+
+        return list(features_map.values())
--- tests/unit/django_transform/test_fields.py
+++ tests/unit/django_transform/test_fields.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
 from unittest import mock
 
 from marshmallow import fields
 
-from flag_engine.django_transform.fields import DjangoRelatedManagerField
+from flag_engine.django_transform.fields import (
+    DjangoFeatureStatesRelatedManagerField,
+    DjangoRelatedManagerField,
+)
 
 
 def test_django_related_manager_field_serialize():
@@ -58,3 +62,44 @@ def test_django_related_manager_field_uses_filter_function_as_provided():
     # Then
     object_to_serialize.my_attribute.all.assert_called_once()
     assert serialized_data == [3, 4]
+
+
+def test_django_feature_state_related_manager_field_serialize_discards_old_versions():
+    # Given
+    # a mock object to serialize
+    attribute_name = "feature_states"
+    object_to_serialize = mock.MagicMock(spec=[attribute_name])
+
+    # and which has some 'feature states' associated with it in the way that you'd
+    # expect a django object to. Each feature state associated with the same feature
+    # but with incrementing version numbers.
+    feature_id = 1
+    yesterday = datetime.now() - timedelta(days=1)
+    feature_states = [
+        mock.MagicMock(id=i, feature_id=feature_id, version=i, live_from=yesterday)
+        for i in (1, 2, 3, 4, 5)
+    ]
+    getattr(object_to_serialize, attribute_name).all.return_value = feature_states
+
+    # and a filter function which will filter out the last feature state based on it's
+    # id
+    def filter_func(fs):
+        return fs.id < 5
+
+    # and we instantiate the field to test against
+    field = DjangoFeatureStatesRelatedManagerField(
+        fields.Field(), filter_func=filter_func
+    )
+
+    # When
+    # we serialize the field holding the feature states
+    serialized_data = field.serialize(attribute_name, obj=object_to_serialize)
+
+    # Then
+    # the 'feature states' were retrieved as expected
+    getattr(object_to_serialize, attribute_name).all.assert_called_once()
+
+    # and only one 'feature state' is returned which has an id and version of 4 (because
+    # it's the maximum version that was not filtered out by the filter function).
+    assert len(serialized_data) == 1
+    assert serialized_data[0].id == serialized_data[0].version == 4
