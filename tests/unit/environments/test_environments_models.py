d4b2a943ef2181e8ae6d3b24abca779b997276c2
Matthew Elwell <matthewe@solidstategroup.com>
2022-04-20 15:25:31 +0100

    Feature/edge api 29/add integrations data property (#104)
    * Add integrations data property to environment model
    
    * Add docstring

--- tests/unit/environments/test_environments_models.py
+++ tests/unit/environments/test_environments_models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from flag_engine.environments.integrations.models import IntegrationModel
 from flag_engine.environments.models import EnvironmentAPIKeyModel
 
 
@@ -56,3 +57,31 @@ def test_environment_api_key_model_is_valid_is_false_for_non_expired_inactive_ke
         ).is_valid
         is False
     )
+
+
+def test_environment_integrations_data_returns_empty_dict_when_no_integrations(
+    environment,
+):
+    assert environment.integrations_data == {}
+
+
+def test_environment_integrations_data_returns_correct_data_when_multiple_integrations(
+    environment,
+):
+    # Given
+    example_key = "some-key"
+    segment_url = "https://api.segment.com"
+
+    environment.mixpanel_config = IntegrationModel(api_key=example_key)
+    environment.segment_config = IntegrationModel(
+        api_key=example_key, base_url=segment_url
+    )
+
+    # When
+    integrations_data = environment.integrations_data
+
+    # Then
+    assert integrations_data == {
+        "mixpanel_config": {"api_key": example_key, "base_url": None},
+        "segment_config": {"api_key": example_key, "base_url": segment_url},
+    }
