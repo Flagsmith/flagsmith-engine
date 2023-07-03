5b6037c5418d391dc5ace23361b3a8b8ff50a52c
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-07-03 15:04:36 +0100

    fix: use `Config.smart_union` to avoid type coercion for `TraitModel.trait_value`

--- flag_engine/identities/traits/models.py
+++ flag_engine/identities/traits/models.py
@@ -6,3 +6,6 @@ from flag_engine.identities.traits.types import TraitValue
 class TraitModel(BaseModel):
     trait_key: str
     trait_value: TraitValue = ...
+
+    class Config:
+        smart_union: bool = True
