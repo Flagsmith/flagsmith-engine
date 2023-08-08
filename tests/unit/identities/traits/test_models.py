c8c7a31f839fdd01a73067d2bae2f5a40417ce24
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-08-08 10:38:30 +0100

    fix: coerce decimal trait values with 0 exponent to integers

--- tests/unit/identities/traits/test_models.py
+++ tests/unit/identities/traits/test_models.py
@@ -1,10 +1,13 @@
+from decimal import Decimal
+from typing import Union
+
 import pytest
 from pydantic import ValidationError
 
 from flag_engine.identities.traits import models
 
 
-def test_trait_model__invalid_trait_value__raises_expected():
+def test_trait_model__invalid_trait_value__raise_expected() -> None:
     # Given
     invalid_trait_kwargs = {"trait_key": "scream", "trait_value": "A" * 2001}
 
@@ -17,3 +20,25 @@ def test_trait_model__invalid_trait_value__raises_expected():
         exc_info.value.errors()[-1]["msg"]
         == "ensure this value has at most 2000 characters"
     )
+
+
+@pytest.mark.parametrize(
+    "trait_value_argument, expected_trait_value",
+    [
+        (Decimal("1"), 1),
+        (Decimal("1.1"), 1.1),
+    ],
+)
+def test_trait_model__decimal_trait_value__coerce_expected(
+    trait_value_argument: Decimal,
+    expected_trait_value: Union[int, float],
+) -> None:
+    # When
+    trait_model = models.TraitModel(
+        trait_key="test_trait",
+        trait_value=trait_value_argument,
+    )
+
+    # Then
+    assert type(trait_model.trait_value) is type(expected_trait_value)
+    assert trait_model.trait_value == expected_trait_value
