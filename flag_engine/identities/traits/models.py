6f87a13cb87801264d8f37107fbf9036ae1d64c3
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-10-25 16:32:20 +0100

    fix: remove dead code

--- flag_engine/identities/traits/models.py
+++ flag_engine/identities/traits/models.py
@@ -1,15 +1,6 @@
-from typing import Any, Union, get_args
-
 from pydantic import BaseModel, Field
-from typing_extensions import TypeGuard
 
 from flag_engine.identities.traits.types import TraitValue
-
-UnconstrainedTraitValue = Union[None, int, float, bool, str]
-
-
-def _is_trait_value(value: Any) -> TypeGuard[UnconstrainedTraitValue]:
-    return isinstance(value, get_args(UnconstrainedTraitValue))
 
 
 class TraitModel(BaseModel):
