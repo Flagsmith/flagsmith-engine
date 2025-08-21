45849353e3844385ad438c9be4cce1570d4196f1
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-08-21 20:03:39 +0100

    exports

--- flag_engine/engine.py
+++ flag_engine/engine.py
@@ -1,3 +1,9 @@
+from flag_engine.context.types import EvaluationContext
+from flag_engine.result.types import EvaluationResult
 from flag_engine.segments.evaluator import get_evaluation_result
 
-__all__ = ("get_evaluation_result",)
+__all__ = (
+    "EvaluationContext",
+    "EvaluationResult",
+    "get_evaluation_result",
+)
