6f524dd0d935d51334e54b799a89a93b56c7bd4f
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-08-22 18:37:28 +0100

    export `ContextValue`

--- flag_engine/engine.py
+++ flag_engine/engine.py
@@ -1,8 +1,10 @@
 from flag_engine.context.types import EvaluationContext
 from flag_engine.result.types import EvaluationResult
 from flag_engine.segments.evaluator import get_evaluation_result
+from flag_engine.segments.types import ContextValue
 
 __all__ = (
+    "ContextValue",
     "EvaluationContext",
     "EvaluationResult",
     "get_evaluation_result",
